a725de176027e43b9d61f432359f39db653f85fe
Streit-ValerioZorzi <144006537+ZorziV@users.noreply.github.com>
2023-11-30 13:51:14 +0100

    Add files via upload

---
+++ GUI-SetupCleaner/app.js
@@ -0,0 +1,79 @@
+// required parameters to use this code
+// use 'npm install express bodyParser path fs rimraf' to install all this parameters
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
+
+//This code creates an Express application and sets it to run on port 3030.
+const app = express();
+const port = 3030;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/deleteFiles', (req, res) => {
+  const { filePath } = req.body;
+
+  checkDateiUndOrdner(filePath);
+
+  res.json({ message: `Versuche, Datei zu löschen: ${filePath}` });
+});
+
+function deleteRecursive(targetPath) {
+  rimraf.sync(targetPath);
+}
+
+function checkDateiUndOrdner(zielOrdner) {
+  try {
+    const files = fs.readdirSync(zielOrdner);
+
+    files.forEach(datei => {
+      const fullPath = path.join(zielOrdner, datei);
+      const stats = fs.statSync(fullPath);
+
+      const currentDate = new Date();
+      const fileDate = new Date(stats.mtime);
+
+      // Überprüfen, ob das Element eine Textdatei mit der Endung ".sf_export_finished" und der dazugehörige Ordner ist
+      if (
+        stats.isDirectory() &&
+        /^[0-9]+$/g.test(datei) &&
+        fs.existsSync(path.join(zielOrdner, `${datei}.sf_export_finished.txt`))
+      ) {
+        const timeDifference = currentDate.getTime() - fileDate.getTime();
+        const oneDayInMillis = 30 * 24 * 60 * 60 * 1000; // 1 Tag in Millisekunden
+
+        if (timeDifference > oneDayInMillis) {
+          console.log(`Lösche Ordner (älter als 1 Tag): ${datei}`);
+          deleteRecursive(fullPath);
+          console.log(`Ordner gelöscht.`);
+        }
+      } else if (
+        stats.isFile() &&
+        /^[0-9]+\.sf_export_finished\.txt$/g.test(datei)
+      ) {
+        const timeDifference = currentDate.getTime() - fileDate.getTime();
+        const oneDayInMillis =  30 * 24 * 60 * 60 * 1000; // 1 Tag in Millisekunden
+
+        if (timeDifference > oneDayInMillis) {
+          console.log(`Lösche Datei (älter als 1 Tag): ${datei}`);
+          fs.unlinkSync(fullPath);
+          console.log(`Datei und zugehöriger Ordner gelöscht.`);
+        }
+      }
+    });
+
+    console.log('Überprüfung abgeschlossen.');
+  } catch (error) {
+    const errorMessage = `Fehler beim Überprüfen der Dateien: ${error.message}`;
+    console.error(errorMessage);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
+});
+
---
+++ GUI-SetupCleaner/public/js/main.js
@@ -0,0 +1,19 @@
+async function deleteFiles() {
+  const filePath = document.getElementById('filePathInput').value;
+
+  try {
+    const response = await fetch('/deleteFiles', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ filePath }),
+    });
+
+    const result = await response.json();
+    console.log(result.message);
+  } catch (error) {
+    console.error('Fehler beim Löschen der Datei:', error.message);
+  }
+}
+
